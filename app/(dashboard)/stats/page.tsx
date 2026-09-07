e500ca76e5d9b4883b2df98398e2197335764b49
Hcookie101 <emailtohansil@gmail.com>
2026-09-07 12:28:12 -0400

    started working on stats page

--- app/(dashboard)/stats/page.tsx
+++ app/(dashboard)/stats/page.tsx
@@ -2,8 +2,15 @@ import Chat from "@/app/ui/chat";
 
 export default function Stats() {
   return (
-    <div className="flex h-[calc(100vh-40px)]">
-      <div className="flex-1 bg-[#E4E0D8]"></div>
+    <div className="flex h-[calc(100vh-40px)] ">
+      <div className="flex-1 bg-[#C9C6C0] h-full overflow-y-auto grid gap-4 grid-cols-2 grid-rows-[220px_160px_160px_180px_180px] [grid-template-areas:'a_a'_'b_c'_'b_d'_'e_f'_'e_f'] p-4">
+        <div className="[grid-area:a] rounded-xl border border-[#b8b5ae] bg-[#E4E0D8]"></div>
+        <div className="[grid-area:b] rounded-xl border border-[#b8b5ae] bg-[#E4E0D8]"></div>
+        <div className="[grid-area:c] rounded-xl border border-[#b8b5ae] bg-[#E4E0D8]"></div>
+        <div className="[grid-area:d] rounded-xl border border-[#b8b5ae] bg-[#E4E0D8]"></div>
+        <div className="[grid-area:e] rounded-xl border border-[#b8b5ae] bg-[#E4E0D8]"></div>
+        <div className="[grid-area:f] rounded-xl border border-[#b8b5ae] bg-[#E4E0D8]"></div>
+      </div>
       <Chat />
     </div>
   );
